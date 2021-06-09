deae00ac0862ee577a290550d2e85bbd5cd0c96a
Paul Denlinger <pmdenlinger@icloud.com>
2021-06-09 16:23:36 -0700

    Initial Commit

---
+++ learning-app/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  learning-app
+//
+//  Created by user on 6/9/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ learning-app/learning_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  learning_appApp.swift
+//  learning-app
+//
+//  Created by user on 6/9/21.
+//
+
+import SwiftUI
+
+@main
+struct learning_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
